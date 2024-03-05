e154e63e96b210f1d6a8b26fdc64e307f3ff58c5
IdoAbram <125755549+IdoAbram@users.noreply.github.com>
2024-03-05 20:07:07 +0200

    Add files via upload

---
+++ PortScanner.cpp
@@ -0,0 +1,67 @@
+// PortScanner.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <thread>
+#include <vector>
+
+
+#define NumOfPorts 100000
+
+#pragma comment(lib, "Ws2_32.lib")
+
+bool isPortOpen(const std::string& host, int port) {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed." << std::endl;
+        return false;
+    }
+
+    SOCKET socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);//AF_INET - basic address family, SOCK_STREAM - for TCP connection
+    if (socket_fd == 0) {
+        std::cerr << "Error creating socket." << std::endl;
+        WSACleanup();
+        return false;
+    }
+
+    sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    inet_pton(AF_INET, host.c_str(), &server_address.sin_addr);
+
+    if (connect(socket_fd, (struct sockaddr*)&server_address, sizeof(server_address)) == 0) {
+        closesocket(socket_fd);
+        WSACleanup();
+        return true;
+    }
+    else {
+        closesocket(socket_fd);
+        WSACleanup();
+        return false;
+    }
+}
+
+void portScanner(const std::string& host, int port) {
+    if (isPortOpen(host, port)) {
+        std::cout << "Port " << port << " on " << host << " is open." << std::endl;
+    }
+}
+
+int main() {
+    std::string host = "127.0.0.1";
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < NumOfPorts; i++) {
+        threads.emplace_back(portScanner, host, i);
+    }
+
+    // Join all threads to wait for them to finish
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    return 0;
+}
